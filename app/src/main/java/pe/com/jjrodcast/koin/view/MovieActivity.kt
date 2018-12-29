b5e7b2ddd7df3fe32c2b62e2a8c2559062902e74
Jorge <kanh_2832@hotmail.com>
2018-12-29 13:16:00 -0500

    Adding Copyright

--- app/src/main/java/pe/com/jjrodcast/koin/view/MovieActivity.kt
+++ app/src/main/java/pe/com/jjrodcast/koin/view/MovieActivity.kt
@@ -1,3 +1,9 @@
+/*
+ * Developed by Jorge Rodriguez on 29/12/18 01:10 PM
+ * Copyright (c) 2018 . All rights reserved.
+ * Last modified 29/12/18 01:09 PM
+ */
+
 package pe.com.jjrodcast.koin.view
 
 import android.arch.lifecycle.Observer
